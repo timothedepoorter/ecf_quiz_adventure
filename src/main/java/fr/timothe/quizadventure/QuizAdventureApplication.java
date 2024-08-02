3dc5b2c19b34f52e3c0d204692a9ad29fcaa3c48
Timothé DEPOORTER <depoortertimothe1@gmail.com>
2024-08-02 10:11:12 +0200

    init project + connection to mongo database

---
+++ src/main/java/fr/timothe/quizadventure/QuizAdventureApplication.java
@@ -0,0 +1,13 @@
+package fr.timothe.quizadventure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizAdventureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizAdventureApplication.class, args);
+	}
+
+}
